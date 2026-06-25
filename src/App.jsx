34f466c751a15d858830c7516a017dbc437d5c2c
Sushil Timilsina <sushil.timilsina@heraldcollege.edu.np>
2026-06-25 08:00:24 +0545

    Initial commit with sidebar and footer

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+     <Sidebar/>
+     <div className='h-[95vh]'>
+
+     </div>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+<footer className="footer sm:footer-horizontal footer-center bg-amber-500 text-base-content pl-200 h-[5vh] pt-4">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - Summer Class</p>
+  </aside>
+</footer>
+
+
+    );
+}
+
+export default Footer;
